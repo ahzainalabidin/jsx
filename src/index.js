2e2569d76a57e93c9d4cf664969fdbf31e1f8189
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-07 14:20:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+// 1) Import React (to define components) and ReactDOM (to show components in browser) libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 2) Take control of <div> with ID named root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Create a component
+function App() {
+  return <h1>Hi there!</h1>
+}
+
+// 4) Show components on the screen
+root.render(
+  <App />
+);
